ac2fba10eb305d15bc04c37c035e4f55ec4b3c23
Sandhiya.M <Sandhiyam@in.boardex.com>
2019-09-11 16:25:49 +0530

    test from eclipse

--- TestSanGit/src/CheckGit.java
+++ TestSanGit/src/CheckGit.java
@@ -3,7 +3,8 @@ public class CheckGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test from github
+		System.out.println("test from github"); 
+		System.out.println("test from eclipse");
 
 	}
 
